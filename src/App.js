dfc7074ccb4f7d6d2d1c3d14e9d60e01a5854a4d
Suraj Kumar <suraj31kumar1999@gmail.com>
2025-04-28 22:31:39 +0530

    trial 21

--- src/App.js
+++ src/App.js
@@ -1,56 +1,21 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Login from './pages/Login';
-// import Host from './pages/Host';
-// import Client from './pages/Client';
-
-// function App() {
-//   return (
-//     <Router>  {/* Wrap Routes inside BrowserRouter */}
-//       <Routes>
-//         <Route path="/" element={<Login />} />
-//         <Route path="/host/:code" element={<Host />} />
-//         <Route path="/client/:code" element={<Client />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-import React from "react"
-import { BrowserRouter as Router, Routes, Route, Navigate, useParams } from "react-router-dom"
-import Host from "./components/Host"
-import Client from "./components/Client"
-import Home from "./components/Home"
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Host from './pages/Host';
+import Client from './pages/Client';
 
 function App() {
   return (
-    <Router>
+    <Router>  {/* Wrap Routes inside BrowserRouter */}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/room/:code" element={<Room />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/host/:code" element={<Host />} />
+        <Route path="/client/:code" element={<Client />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-function Room() {
-  const params = useParams()
-  const { code } = params
+export default App;
 
-  if (!code) {
-    return <Navigate to="/" replace />
-  }
 
-  // Special case for host
-  if (code === "host") {
-    return <Host />
-  }
-
-  return <Client />
-}
-
-export default App
